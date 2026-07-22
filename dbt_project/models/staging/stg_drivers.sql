1bfe3cf7f46a2a38e3bd040dc070177bc2527edf
duncotieno99@gmail.com <duncotieno99@gmail.com>
2026-07-22 19:14:27 +0000

    drivers_raw into staging

---
+++ dbt_project/models/staging/stg_drivers.sql
@@ -0,0 +1,25 @@
+WITH deduped AS(
+    SELECT
+        code AS driver_code,
+        driverId AS driver_id,
+        familyName AS family_name,
+        givenName AS given_name,
+        nationality,
+        season,
+        url AS driver_url,
+        CAST(dateofBirth AS DATE) AS date_of_birth,
+        CAST(permanentNumber AS int) AS permanent_number,
+        ROW_NUMBER() OVER(PARTITION BY driverId ORDER BY season DESC) AS rn
+    FROM {{ source('bronze', 'drivers_raw') }}
+)
+SELECT 
+    driver_id,
+    driver_code,
+    given_name,
+    family_name,
+    date_of_birth,
+    nationality,
+    permanent_number,
+    driver_url
+FROM deduped
+WHERE rn = 1;
